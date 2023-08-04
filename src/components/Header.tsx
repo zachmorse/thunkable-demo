fcda6e39acc77b7ccc2b2b807e11617f7cda9af8
Zachary Morse <zacharymorse@gmail.com>
2023-08-04 05:40:32 -0400

    make header decent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import styled from '@emotion/styled'
+import { Button, Fab } from '@mui/material'
 
 import Beaver from '../assets/ThunkableBeaver.png'
-import PlusSign from '../assets/plus_sign.svg'
+// import PlusSign from '../assets/plus_sign.svg'
+import PlusSign from '../assets/PlusSign'
 
 import { createProjectDraft } from '../store/actions/list'
 
@@ -11,18 +13,29 @@ const Banner = styled.div({
     backgroundColor: '#F7F9FD',
     fontSize: '1rem',
     color: '#424242',
-    width: '100%',
+    margin: '0rem auto 2rem',
+    width: '85%',
     textTransform: 'uppercase',
     height: '7.75rem',
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     gridTemplateRows: '1fr 1fr',
-    marginBottom: '2rem'
+    marginBottom: '3rem',
+    borderBottom: '1px solid rgba(0,0,0,0.1)',
+    boxSizing: 'border-box',
+    padding: '0rem 0.5rem'
 })
 
 const LogoContainer = styled.div({
     width: '2rem',
-    height: '2rem'
+    height: '2rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '& img': {
+        marginTop: '1rem',
+        width: '2rem'
+    }
 })
 
 const AppTitle = styled.div({
@@ -30,13 +43,21 @@ const AppTitle = styled.div({
     alignItems: 'flex-end',
     paddingLeft: '0rem 0rem 1rem 1rem',
     letterSpacing: '0.8 px',
-    gridRow: 2
+    gridRow: 2,
+    marginBottom: '0.5rem'
 })
 
-const NewProjectButton = styled.button({
-    gridRow: 2,
+const ButtonContainer = styled.div({
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'flex',
     gridColumn: 2,
-    width: 'fit-content'
+    gridRow: 2,
+    '& span': {
+        position: 'relative',
+        right: 20,
+        top: 34
+    }
 })
 
 interface iHeader {
@@ -49,8 +70,21 @@ const Header = ({ createProjectDraft }: iHeader) => {
             <LogoContainer>
                 <img src={Beaver} />
             </LogoContainer>
-            <NewProjectButton onClick={createProjectDraft}>add new project</NewProjectButton>
-            {/* ZCM: replace this obviously ^^^ */}
+            <ButtonContainer>
+                <span>
+                    <Fab
+                        onClick={createProjectDraft}
+                        sx={{
+                            transition: 'all 125ms',
+                            backgroundColor: '#4A475F',
+                            ':hover': { backgroundColor: '#3D3A4F' }
+                        }}
+                    >
+                        <PlusSign />
+                    </Fab>
+                </span>
+            </ButtonContainer>
+
             <AppTitle>My Projects</AppTitle>
         </Banner>
     )
